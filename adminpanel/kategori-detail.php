e35e3ee320260191e29637ca87a09804f3ca9306
amaliyahnurhaida <amaliyahnurhaida@gmail.comgit config --global user.email amaliyahnurhaida@gmail.comgit config --global user.email amaliyahnurhaida@gmail.com>
2024-06-07 17:28:13 +0700

    merapikan tubes

--- adminpanel/kategori-detail.php
+++ adminpanel/kategori-detail.php
@@ -18,6 +18,7 @@ $data = mysqli_fetch_array($query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
     <!-- css -->
     <link rel="stylesheet" href="../css/style.css">
     <!-- font -->
